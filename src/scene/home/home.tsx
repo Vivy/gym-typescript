f9a401ae6fcf1aee51ff3298350fad7650f74b4d
Vivy <Tomitaviorel@yahoo.com>
2023-02-07 18:32:43 +0200

    red

--- src/scene/home/home.tsx
+++ src/scene/home/home.tsx
@@ -3,7 +3,7 @@ import useMediaQuery from '@/hooks/usemadiaquerie';
 import ActionButton from '@/shared/actionbutton';
 import HomePageText from '@/assets/HomePageText.png';
 import HomePageGraphic from '@/assets/HomePageGraphic.png';
-import SponsorRedbull from '@/assets/SponsorRedBull.png';
+import SponsorRedBull from '@/assets/SponsorRedBull.png';
 import SponsorForbes from '@/assets/SponsorForbes.png';
 import SponsorFortune from '@/assets/SponsorFortune.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
@@ -86,7 +86,7 @@ const Home = ({ setSelectedPage }: Props) => {
         <div className='h-[150px] w-full bg-primary-100 py-10'>
           <div className='mx-auto w-5/6'>
             <div className='item-center flex  w-3/5 justify-between gap-8'>
-              <img src={SponsorRedbull} alt='redbull-sponsor' />
+              <img src={SponsorRedBull} alt='redbull-sponsor' />
               <img src={SponsorForbes} alt='forbes-sponsor' />
               <img src={SponsorFortune} alt='fortune-sponsor' />
             </div>
