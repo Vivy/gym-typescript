f701809bb465e4dd7a66123786490ff4b1b90c91
Vivy <Tomitaviorel@yahoo.com>
2023-02-07 14:10:05 +0200

    feat: mobile burger works

--- src/scene/navbar/index.tsx
+++ src/scene/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from '@/assets/Logo.png';
 import Link from './link';
 import { SelectedPage } from '@/shared/types';
 import useMediaQuery from '@/hooks/usemadiaquerie';
+import ActionButton from '@/shared/actionbutton';
 
 type Props = {
   selectedPage: SelectedPage;
@@ -47,13 +48,15 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Become a Member</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
                 </div>
               </div>
             ) : (
               <button
                 className='rounded-full bg-secondary-500 p-2 '
-                onClick={() => setIsMenuToggle(!setIsMenuToggle)}
+                onClick={() => setIsMenuToggle(!isMenuToggle)}
               >
                 <Bars3Icon className='h-6 w-6 text-white ' />
               </button>
@@ -61,6 +64,42 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+
+      {/*Mobile Menu Modal*/}
+      {!isAboveMediumScreens && isMenuToggle && (
+        <div className='fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl'>
+          {/* {Close Icon} */}
+          <div className='flex justify-end p-12'>
+            <button onClick={() => setIsMenuToggle(!isMenuToggle)}>
+              <XMarkIcon className='h-6 w-6 text-gray-400' />
+            </button>
+          </div>
+
+          {/* {Menu Items} */}
+          <div className='ml-[33%] flex flex-col gap-10 text-2xl'>
+            <Link
+              page='Home'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Benefits'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Our Classes'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Contact Us'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
