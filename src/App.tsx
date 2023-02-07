ca5fc9f52dc4f9b144beb53bf39b5df1075195a7
Vivy <Tomitaviorel@yahoo.com>
2023-02-07 12:32:15 +0200

    feat: refactore some ccode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div className='App'>app</div>;
-}
+import Navbar from './scene/navbar';
+
+const App = () => {
+  return (
+    <div className='app bg-gray-20'>
+      <Navbar />
+    </div>
+  );
+};
 
 export default App;
